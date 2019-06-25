d1893abd876e957e5a53b2fe1bf1b28c8082d630
Lisa <Eleriya-25@yandex.ru>
2019-06-25 20:54:07 +0700

    Создание приложения  core
     Добавили в папку проекта новое приложение и привязали его через файлы настроек и маршрутов.

--- django/config/urls.py
+++ django/config/urls.py
@@ -4,6 +4,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='layout/wrapper.html'), name='main_page'),
+    path('core/', include('core.urls', namespace='core')),
     path('social/', include('social_django.urls', namespace='social')),
     path('accounts/', include('user.urls', namespace='user')),
     path('admin/', admin.site.urls),
---
+++ django/core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from core.views import *
+
+# comon_prefix: core/'
+app_name = 'core'
+urlpatterns = [
+    #path('login/', LoginView.as_view(), name='login'),
+]
